cb5f72cbb350e96f16017465d78c97239ba5d3e9
teko <rshlonchak@gmail.com>
2022-11-21 15:47:04 +0200

    added display of CRUD implementation in the browser for categories

--- src/main/java/com/example/infogame/controllers/CategoryController.java
+++ src/main/java/com/example/infogame/controllers/CategoryController.java
@@ -1,16 +1,16 @@
 package com.example.infogame.controllers;
 
 import com.example.infogame.dto.category.CategoryCreateDto;
-import com.example.infogame.dto.category.CategoryResponseDto;
 import com.example.infogame.dto.category.CategoryUpdateDto;
 import com.example.infogame.service.CategoryService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Tag(name = "Categories")
-@RestController
+@Controller
+@RequestMapping("/category")
 public class CategoryController {
 
     private final CategoryService categoryService;
@@ -19,28 +19,70 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("/categories")
-    public List<CategoryResponseDto> listCategories() {
-        return categoryService.getCategory();
+//    @GetMapping("/categories")
+//    public List<CategoryResponseDto> listCategories() {
+//        return categoryService.getCategory();
+//    }
+
+    @GetMapping()
+    public String listCategories(Model model) {
+        model.addAttribute("categories", categoryService.getCategory());
+        return "category/list";
     }
 
-    @PostMapping("/categories")
-    public CategoryResponseDto createCategory(@RequestBody CategoryCreateDto categoryCreateDto) {
-        return categoryService.createCategory(categoryCreateDto);
+//    @PostMapping("/categories")
+//    public CategoryResponseDto createCategory(@RequestBody CategoryCreateDto categoryCreateDto) {
+//        return categoryService.createCategory(categoryCreateDto);
+//    }
+
+    @GetMapping("/new")
+    public String newCategory(Model model) {
+        model.addAttribute("category", new CategoryCreateDto());
+        return "category/new";
     }
 
-    @GetMapping("/category/{categoryId}")
-    public CategoryResponseDto getCategory(@PathVariable("categoryId") int categoryId) {
-        return categoryService.getCategoryById(categoryId);
+    @PostMapping()
+    public String createCategory(@ModelAttribute CategoryCreateDto categoryCreateDto) {
+        categoryService.createCategory(categoryCreateDto);
+        return "redirect:/category";
     }
 
-    @PutMapping("/category/{categoryId}")
-    public CategoryResponseDto updateCategory(@PathVariable("categoryId") int categoryId, @RequestBody CategoryUpdateDto categoryUpdateDto) {
-        return categoryService.updateCategory(categoryId, categoryUpdateDto);
+//    @GetMapping("/category/{categoryId}")
+//    public CategoryResponseDto getCategory(@PathVariable("categoryId") int categoryId) {
+//        return categoryService.getCategoryById(categoryId);
+//    }
+
+    @GetMapping("/{categoryId}")
+    public String getCategory(@PathVariable("categoryId") int categoryId, Model model) {
+        model.addAttribute("category", categoryService.getCategoryById(categoryId));
+        return "category/profile";
     }
 
-    @DeleteMapping("/category/{categoryId}")
-    public void deleteCategory(@PathVariable("categoryId") int categoryId) {
+//    @PutMapping("/category/{categoryId}")
+//    public CategoryResponseDto updateCategory(@PathVariable("categoryId") int categoryId, @RequestBody CategoryUpdateDto categoryUpdateDto) {
+//        return categoryService.updateCategory(categoryId, categoryUpdateDto);
+//    }
+
+    @GetMapping("/{categoryId}/edit")
+    public String editCategory(@PathVariable("categoryId") int categoryId, Model model) {
+        model.addAttribute("category", categoryService.getCategoryById(categoryId));
+        return "category/edit";
+    }
+
+    @PutMapping("/{categoryId}")
+    public String updateCategory(@PathVariable("categoryId") int categoryId, @ModelAttribute("category") CategoryUpdateDto categoryUpdateDto) {
+        categoryService.updateCategory(categoryId, categoryUpdateDto);
+        return "redirect:/category/{categoryId}";
+    }
+
+//    @DeleteMapping("/category/{categoryId}")
+//    public void deleteCategory(@PathVariable("categoryId") int categoryId) {
+//        categoryService.deleteCategory(categoryId);
+//    }
+
+    @DeleteMapping("/{categoryId}")
+    public String deleteCategory(@PathVariable("categoryId") int categoryId) {
         categoryService.deleteCategory(categoryId);
+        return "redirect:/category";
     }
 }
